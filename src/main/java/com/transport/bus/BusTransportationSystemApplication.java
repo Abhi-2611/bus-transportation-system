bc61da047eb841c99aa5708b113f8d438eba34a6
Abhishek Musale <abhimusale2631@gmail.com>
2026-03-12 23:37:46 +0530

    updated ymal and App

--- src/main/java/com/transport/bus/BusTransportationSystemApplication.java
+++ src/main/java/com/transport/bus/BusTransportationSystemApplication.java
@@ -8,6 +8,10 @@ public class BusTransportationSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BusTransportationSystemApplication.class, args);
+
+		System.out.println("----------------------------------------------------------------------------");
+		System.out.println("                Bus Transportation System Application Running               ");
+		System.out.println("----------------------------------------------------------------------------");
 	}
 
 }
