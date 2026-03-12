a478cda3802be679d35df6e73ad00c79e7296612
Abhishek Musale <abhimusale2631@gmail.com>
2026-03-12 21:20:05 +0530

    Initial Spring Boot project structure

---
+++ src/main/java/com/transport/bus/BusTransportationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.transport.bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTransportationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusTransportationSystemApplication.class, args);
+	}
+
+}
