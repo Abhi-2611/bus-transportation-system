f657ff9c5f9c919a13013f76d580ab5a99f3787d
Abhishek Musale <abhimusale2631@gmail.com>
2026-03-14 11:18:18 +0530

    add OpenAPI configuration for API documentation

---
+++ src/main/java/com/transport/bus/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.transport.bus.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI busTransportationAPI() {
+
+        Contact contact = new Contact();
+        contact.setName("Bus Transportation System");
+        contact.setEmail("support@transport.com");
+
+        Info info = new Info()
+                .title("Bus Transportation System API")
+                .version("1.0")
+                .description("API documentation for Bus Transportation System backend")
+                .contact(contact);
+
+        return new OpenAPI().info(info);
+    }
+}
+
+
+// http://localhost:10002/swagger-ui/index.html
