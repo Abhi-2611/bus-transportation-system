ce0f340a3dcdf409e728c383e58548b3649b6cd2
Abhishek Musale <124806834+Abhi-2611@users.noreply.github.com>
2026-03-14 15:46:28 +0530

    Comment out OpenApiConfig class and methods
    we can't use openAPI with springboot version 4.x

--- src/main/java/com/transport/bus/config/OpenApiConfig.java
+++ src/main/java/com/transport/bus/config/OpenApiConfig.java
@@ -1,31 +1,31 @@
-package com.transport.bus.config;
+// package com.transport.bus.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
+// import io.swagger.v3.oas.models.OpenAPI;
+// import io.swagger.v3.oas.models.info.Contact;
+// import io.swagger.v3.oas.models.info.Info;
 
-@Configuration
-public class OpenApiConfig {
+// @Configuration
+// public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI busTransportationAPI() {
+//     @Bean
+//     public OpenAPI busTransportationAPI() {
 
-        Contact contact = new Contact();
-        contact.setName("Bus Transportation System");
-        contact.setEmail("support@transport.com");
+//         Contact contact = new Contact();
+//         contact.setName("Bus Transportation System");
+//         contact.setEmail("support@transport.com");
 
-        Info info = new Info()
-                .title("Bus Transportation System API")
-                .version("1.0")
-                .description("API documentation for Bus Transportation System backend")
-                .contact(contact);
+//         Info info = new Info()
+//                 .title("Bus Transportation System API")
+//                 .version("1.0")
+//                 .description("API documentation for Bus Transportation System backend")
+//                 .contact(contact);
 
-        return new OpenAPI().info(info);
-    }
-}
+//         return new OpenAPI().info(info);
+//     }
+// }
 
 
-// http://localhost:10002/swagger-ui/index.html
+// // http://localhost:10002/swagger-ui/index.html
